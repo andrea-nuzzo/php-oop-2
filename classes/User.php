a724a6a2c7b9522c24c9dcd2e7932121c1b9fa1d
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-01-31 15:48:21 +0100

    draft users

---
+++ classes/User.php
@@ -0,0 +1,42 @@
+<?php
+    class User {
+        private $lastName;
+        private $fisrtName;
+        private $userName;
+        private $email;
+        private $password;
+
+        public function __construct($_userName, $_email, $_password)
+        {
+            $this->userName =  $this->setUserName($_userName);
+            $this->email = $this->setEmail($_email);
+            $this->password = $this->setPassword($_password);
+        }
+         //Funzione che controlla il valore userName
+        public function setUserName($_userName){
+          if(strlen($_userName) < 4 ){
+              throw new Exception("The username must contain at least 4 characters");
+          } else{
+              return $_userName;
+          }
+        }
+        //Funzione che controlla il valore email
+        public function setEmail($_email){
+            if(!strpos($_email, "@") && !strpos($_email, ".")){
+                throw new Exception("The email must contain at least one at and one dot");
+            }else{
+                return $_email;
+            }
+        }
+        //Funzione che controlla il valore password
+        public function setPassword($_password){
+            return $_password;
+        }
+        
+        /***** NATURALMENTE TUTTI QUESTI VAOLORI DOVREBBERO ESSERE $_POST[] POSTATI ALL'INTERNO DI UN DB E CONTROLLARE CHE EMAIL E USERNAME SIANO UNIQUE*****/
+       
+
+
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
+    require_once __DIR__ . "/classes/Product.php";
+    require_once __DIR__ . "/classes/User.php";
+
+try{
+    $users = [
+        new User("wolverinenetwork","sibizoddozi-9649@yopmail.com","ND%gM"),
+        new User("ranran","frauffoveujeugi-1122@yopmail.com","s%s4J"),
+        new User("olaf","widdinneimuqui-3416@yopmail.com","7ke&J")
+    ];
+}catch (Exception $e){
+    echo "An exception was raised: *** {$e->getMessage()} ***";
+}
+    var_dump($users);
 ?>
+
+
+<!-- ****CERCARE SOLUZIONE PER SOLLEVARE ECCEZIONI MULTIPLE **** -->
